3230f48c03097e9bdf7fe79693c5b3cdc6bc4def
champmahfuz <mahfuzrahman517@gmail.com>
2022-01-27 15:55:44 +0600

    added code

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
-//middleware here
+//middleware here starts
 app.use(cors());
 app.use(express.json());
 
